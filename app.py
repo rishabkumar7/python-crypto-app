1c7097d4e1e22c26d742e3182a87597df264d9e6
Rishab Kumar <rishabkumar7@gmail.com>
2022-11-01 21:18:29 +0000

    crypto app with table trending prizes

---
+++ app.py
@@ -0,0 +1,17 @@
+import json
+import os
+import requests
+from flask import Flask
+from flask import flash, render_template, request, redirect
+from pycoingecko import CoinGeckoAPI
+
+cg = CoinGeckoAPI()
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    crypto_prices = cg.get_search_trending()
+    crypto_prices = crypto_prices['coins']
+    print(crypto_prices[0])
+    return render_template('index.html', crypto_prices=crypto_prices)
